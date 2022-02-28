6aae31285bd3a6de3f0220cd8ee72bbce5500c0d
Mikaela <bergm@mail.gvsu.edu>
2022-02-28 00:34:18 +0000

    App progress. Setting up the UI and some server stuff

--- ShinyTree/app.R
+++ ShinyTree/app.R
@@ -1,49 +1,106 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(bslib)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+    theme = bs_theme(version = 5, bootswatch = "cosmo"),
+    
+    navbarPage( "Project Shiny Tree",
+        tabPanel("Home", 
+            #Side Bar for Home Page
+                sidebarLayout(
+                    # Sidebar panel for inputs ----
+                    sidebarPanel(
+                        # Input: Select a file ----
+                        fileInput("file1", "Choose CSV File",
+                                  multiple = TRUE,
+                                  accept = c("text/csv",
+                                             "text/comma-separated-values,text/plain",
+                                             ".csv")),
+                        # Horizontal line ----
+                        tags$hr(),
+                        # Input: Checkbox if file has header ----
+                        checkboxInput("header", "Header", TRUE),
+                        # Input: Choose dataset ----
+                        selectInput("dataset", "Choose a dataset:",
+                                    choices = c("rock", "pressure", "cars")),
+                        # Button
+                        downloadButton("downloadData", "Download")
+                    ),
+                ),
+            # Main panel for displaying outputs ----
+            mainPanel(
+                # Output: Data file ----
+                tableOutput("contents"),
+                
+                tableOutput("table"),
+                
+                textInput("text", label = h3("Text input"), value = "Enter text..."),
+                hr(),
+                fluidRow(column(3, verbatimTextOutput("value"))),
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+        tabPanel("About"),
+        tabPanel("Resoutces")
+    ), 
 )
+    
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # Reactive value for selected dataset ----
+    datasetInput <- reactive({
+        switch(input$dataset,
+               "rock" = rock,
+               "pressure" = pressure,
+               "cars" = cars)
+    })
+    
+    # Table of selected dataset ----
+    output$table <- renderTable({
+        datasetInput()
+    })
+    
+    # Downloadable csv of selected dataset ----
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            paste(input$dataset, ".csv", sep = "")
+        },
+        content = function(file) {
+            write.csv(datasetInput(), file, row.names = FALSE)
+        }
+    )
+    
+    output$contents <- renderTable({
+        
+        # input$file1 will be NULL initially. After the user selects
+        # and uploads a file, head of that data file by default,
+        # or all rows if selected, will be shown.
+        
+        req(input$file1)
+        
+        # when reading semicolon separated files,
+        # having a comma separator causes `read.csv` to error
+        tryCatch(
+            {
+                df <- read.csv(input$file1$datapath,
+                               header = input$header,
+                               sep = input$sep,
+                               quote = input$quote)
+            },
+            error = function(e) {
+                # return a safeError if a parsing error occurs
+                stop(safeError(e))
+            }
+        )
+        
+        if(input$disp == "head") {
+            return(head(df))
+        }
+        else {
+            return(df)
+        }
+        
     })
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+shinyApp(ui, server)
+
